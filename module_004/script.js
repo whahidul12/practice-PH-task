28b41c089c50cb918bc3f44dd26daf6b4befec4a
whahidul12 <mdislamwhahid@gmail.com>
2025-08-18 23:24:30 +0600

    first commit

---
+++ module_004/script.js
@@ -0,0 +1,12 @@
+// document.addEventListener('contextmenu', event => event.preventDefault());
+// document.addEventListener('keydown', function (e) {
+//   if (e.ctrlKey && (e.key === 'u' || e.key === 'c' || e.key === 's')) {
+//     e.preventDefault();
+//   }
+// });
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+// document.addEventListener('DOMContentLoaded', function () {
+//   document.body.onselectstart = function () { return false; };
+//   document.body.onmousedown = function () { return false; };
+// });
